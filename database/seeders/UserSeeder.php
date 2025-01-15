b68fb2f162eb349508b745692d796be3cb513f86
Blemnz <nizamuddinfaisal25@gmail.com>
2025-01-15 13:58:21 +0700

    second commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12332112')
+        ]);
+
+        $admin->assignRole('superAdmin');
     }
 }
